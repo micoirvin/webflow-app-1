3c40d2d0b13ee0ecd803f71e62dc96e4b45384f2
micoshortpoint <123370440+micoshortpoint@users.noreply.github.com>
2023-11-27 11:16:28 +0800

    Testing to change words

--- public/index.js
+++ public/index.js
@@ -9,37 +9,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 document.getElementById("lorem").onsubmit = (event) => __awaiter(this, void 0, void 0, function* () {
     event.preventDefault();
-    const allElements = yield webflow.getAllElements();
-    const strings = allElements.flatMap(el => el.type === "String" ? [el] : []);
-    // document.getElementById("text").innerText = JSON.stringify(divs);
-    function correctText(text) {
-        return text.replaceAll(/Office(?=([ -]*365))/ig, "Microsoft");
-    }
-    function correctPageTexts(page) {
+    correctPageTexts();
+});
+function correctText(text) {
+    return text.replaceAll(/Office(?=([ -]*365))/gi, "Microsoft");
+}
+function correctPageTexts() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const page = yield webflow.getCurrentPage();
         let name = page.getName();
         let title = page.getTitle();
         let desc = page.getDescription();
         let slug = page.getSlug();
         console.log(name, title, desc, slug);
-        // page.setName(correctText(name));
-        // page.setTitle(correctText(title));
+        page.setName(correctText(name));
+        page.setTitle(correctText(title));
         page.setDescription(correctText(desc));
-        // page.setSlug(correctText(slug));
-        page.save().then(() => console.log("OKAY"));
-    }
-    strings.forEach((el) => __awaiter(this, void 0, void 0, function* () {
-        let text = el.getText();
-        console.log(el, el.type, text);
-        el.setText(correctText(text));
-        yield el.save();
-    }));
-    // const items = await webflow.getAllPagesAndFolders();
-    // items.forEach((item) => {
-    //   // Process each page or folder
-    //   if (item.type === "Page") {
-    //     correctPageTexts(item);
-    //   }
-    // });
-    const currentPage = yield webflow.getCurrentPage();
-    correctPageTexts(currentPage);
-});
+        page.setSlug(correctText(slug));
+        page.save().then(() => console.log("Page SEO fields changed"));
+        const allElements = yield webflow.getAllElements();
+        const strings = allElements.flatMap((el) => el.type === "String" ? [el] : []);
+        strings.forEach((el, i) => __awaiter(this, void 0, void 0, function* () {
+            let text = el.getText();
+            console.log(el, el.type, text);
+            el.setText(correctText(text));
+            yield el.save();
+        }));
+    });
+}
+// async function loopThroughPages(i = 1) {
+//   console.log(i)
+//   const items = await webflow.getAllPagesAndFolders();
+//   const item = items[i];
+//   if (item.type === "Page") {
+//     i++;
+//     await switchPageAndDo(item, correctPageTexts, i);
+//   }
+// }
+// async function switchPageAndDo(page, func, i) {
+//   return new Promise(async function (resolve) {
+//     await webflow.switchPage(page);
+//     const success = await func.apply();
+//     resolve(success);
+//   }).then(value => loopThroughPages(i));
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,54 @@
 document.getElementById("lorem").onsubmit = async (event) => {
   event.preventDefault();
-  const allElements = await webflow.getAllElements();
-  const strings = allElements.flatMap(el => el.type === "String" ? [el] : []);
-  // document.getElementById("text").innerText = JSON.stringify(divs);
+  correctPageTexts();
+};
 
-  function correctText(text) {
-    return text.replaceAll(/Office(?=([ -]*365))/ig, "Microsoft");
-  }
+function correctText(text) {
+  return text.replaceAll(/Office(?=([ -]*365))/gi, "Microsoft");
+}
 
-  function correctPageTexts(page) {
+async function correctPageTexts() {
+    const page = await webflow.getCurrentPage();
     let name = page.getName();
     let title = page.getTitle();
     let desc = page.getDescription();
     let slug = page.getSlug();
-    console.log(name, title, desc, slug)
-    // page.setName(correctText(name));
-    // page.setTitle(correctText(title));
+    console.log(name, title, desc, slug);
+  
+    page.setName(correctText(name));
+    page.setTitle(correctText(title));
     page.setDescription(correctText(desc));
-    // page.setSlug(correctText(slug));
-    page.save().then(() => console.log("OKAY"))
-  }
+    page.setSlug(correctText(slug));
+    page.save().then(() => console.log("Page SEO fields changed"));
+  
+    const allElements = await webflow.getAllElements();
+    const strings = allElements.flatMap((el) =>
+      el.type === "String" ? [el] : []
+    );
+  
+    strings.forEach(async (el, i) => {
+      let text = el.getText();
+      console.log(el, el.type, text);
+      el.setText(correctText(text));
+      await el.save();
+    });
 
-  strings.forEach(async (el) => {
-    let text = el.getText();
-    console.log(el, el.type, text)
-    el.setText(correctText(text));
-    await el.save();
-  });
+}
 
-  // const items = await webflow.getAllPagesAndFolders();
-  // items.forEach((item) => {
-  //   // Process each page or folder
-  //   if (item.type === "Page") {
-  //     correctPageTexts(item);
-  //   }
-  // });
+// async function loopThroughPages(i = 1) {
+//   console.log(i)
+//   const items = await webflow.getAllPagesAndFolders();
+//   const item = items[i];
+//   if (item.type === "Page") {
+//     i++;
+//     await switchPageAndDo(item, correctPageTexts, i);
+//   }
+// }
 
-  const currentPage = await webflow.getCurrentPage();
-  correctPageTexts(currentPage)
-};
+// async function switchPageAndDo(page, func, i) {
+//   return new Promise(async function (resolve) {
+//     await webflow.switchPage(page);
+//     const success = await func.apply();
+//     resolve(success);
+//   }).then(value => loopThroughPages(i));
+// }
